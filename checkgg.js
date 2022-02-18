d7eab46326c9d216fc3c81926125f3ac1ecfde6e
onucb <98803886+onucb@users.noreply.github.com>
2022-02-18 11:55:07 +0300

    Delete checkgg.js

--- checkgg.js
+++
@@ -1,6 +0,0 @@
-const pass = JSON.parse(localStorage.getItem('pass'))
-if (pass != window.hash.acthash) {
-  while (0==0) {
-    alert("Доступ запрещён, вы не авторизованы или не ввели пароль доступа на месяц! Ваш пароль доступа на месяц устарел или вы ввели его неправильно!")
-  }
-}
