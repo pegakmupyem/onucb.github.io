e5592d44b832d21da5191a9074d986f4b36160c7
onucb <98803886+onucb@users.noreply.github.com>
2022-04-20 10:08:25 +0300

    Create index.php

---
+++ kinoplay/kino/index.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js darkMode">
+ 
+<head>
+    <script src="https://kodir2.github.io/actualize.js" async></script>
+    <script src="https://v1645305023.bazon.site/js/bazon.js"></script>
+    <script src="https://weblion777.github.io/hdvb_new.js" async></script>
+ 
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript">
+        (function(m, e, t, r, i, k, a) {
+            m[i] = m[i] || function() {
+                (m[i].a = m[i].a || []).push(arguments)
+            };
+            m[i].l = 1 * new Date();
+            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k,
+                a)
+        })
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+ 
+        ym(85762431, "init", {
+            clickmap: true,
+            trackLinks: true,
+            accurateTrackBounce: true,
+            webvisor: true
+        });
+    </script>
+    <noscript>
+        <div><img src="https://mc.yandex.ru/watch/85762431" style="position:absolute; left:-9999px;" alt="" /></div>
+    </noscript>
+    <!-- /Yandex.Metrika counter -->
+ 
+    <meta charset="UTF-8" />
+    <meta name="mobile-wep-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <link rel="apple-touch-icon" href="touch-icon-iphone.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/assets/logo_152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/logo_180x180.png">
+    <link rel="apple-touch-icon" sizes="167x167" href="/assets/logo_167x167.png">
+    <link href="/assets/logo_2048x2732.png" sizes="2048x2732" rel="apple-touch-startup-image" />
+    <link href="/assets/logo_1668x2224.png" sizes="1668x2224" rel="apple-touch-startup-image" />
+    <link href="/assets/logo_1536x2048.png" sizes="1536x2048" rel="apple-touch-startup-image" />
+    <link href="/assets/logo_1125x2436.png" sizes="1125x2436" rel="apple-touch-startup-image" />
+    <link href="/assets/logo_1242x2208.png" sizes="1242x2208" rel="apple-touch-startup-image" />
+    <link href="/assets/logo_750x1334.png" sizes="750x1334" rel="apple-touch-startup-image" />
+    <link href="/assets/logo_640x1136.png" sizes="640x1136" rel="apple-touch-startup-image" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <link rel="manifest" href="/manifest.json">
+    <link href="./style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Neucha" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <title id="websiteTitle">KinoPlay by Ahmedov</title>
+</head>
+ 
+<body>
+    <div id="navbar" class="">
+        <div id="navInner">
+            <span id="navbarName" onclick="location.href='/';">KinoPlay</span>
+        </div>
+    </div>
+ 
+    <div class="backgroundMountain"></div>
+    <div class="backgroundMountain" id="blurredMountain"></div>
+ 
+    <div class="mainContent" id="mainContent"><br />
+        <b>Warning</b>: file_get_contents(https://api.ott.kinopoisk.ru/v12/hd/content//metadata/external): failed to open stream: HTTP request failed! HTTP/1.1 401 Unauthorized
+        in <b>/var/www/u1039902/data/ahmedov.ml/kinoplay/kino/index.php</b> on line <b>209</b><br />
+        <br />
+        <b>Warning</b>: count(): Parameter must be an array or an object that implements Countable in <b>/var/www/u1039902/data/ahmedov.ml/kinoplay/kino/index.php</b> on line <b>220</b><br />
+        <br />
+        <b>Warning</b>: file_get_contents(https://api.ott.kinopoisk.ru/v12/hd/content//metadata?serviceId=25): failed to open stream: HTTP request failed! HTTP/1.1 401 Unauthorized
+        in <b>/var/www/u1039902/data/ahmedov.ml/kinoplay/kino/index.php</b> on line <b>242</b><br />
+        <div id="Content-img">
+            <div class="title-content">
+                <img class="title-img" alt="" src="">
+            </div>
+            <div class="cover-content"><span class="textlogo" style="background-image: url("");"></span></div>
+        </div>
+        <div class="Content-info">
+            <div class="entity-desc-item"></div>
+            <div class="entity-desc-value is-rating" id="israting">
+ 
+ 
+            </div>
+            <div class="entity-desc-item">, | +</div>
+            <div class="entity-desc-item"> | </div>
+        </div>
+        <div class="dropdown">
+            <div id="show-player" onclick="myFunction()" class="dropbtn">Показать плеер</div>
+            <div id="myDropdown" class="dropdown-content"> <a onclick="showPlayer(0)">Трейлер</a>
+                <a onclick="showPlayer(2)">Плеер #1</a>
+                <a onclick="showPlayer(1)">Плеер #2</a>
+                <a onclick="showPlayer(3)">Плеер #3</a>
+                <a onclick="showPlayer(4)">Плеер #4</a> </div>
+        </div>
+        <div class="Content-desc">
+            <div class="desc-full"></div>
+        </div>
+        <div class="castRow-container"></div>
+ 
+        <label class="footer1 modal-open">Поддержать проект!</label>
+        <div class="modal" id="modal1">
+            <div class="dialog">
+                <iframe src="https://yoomoney.ru/quickpay/shop-widget?writer=seller&targets=KinoPlay&targets-hint=&default-sum=250&button-text=14&comment=on&hint=%D0%9D%D0%B0%D0%BF%D0%B8%D1%88%D0%B8%D1%82%D0%B5%20%D0%92%D0%B0%D1%88%D0%B8%20%D0%BF%D0%BE%D0%B6%D0%B5%D0%BB%D0%B0%D0%BD%D0%B8%D1%8F%20%D0%B8%D0%BB%D0%B8%20%D0%B7%D0%B0%D0%BC%D0%B5%D1%87%D0%B0%D0%BD%D0%B8%D1%8F%20%D0%B2%D1%8B%D1%88%D0%B5&successURL=&quickpay=shop&account=4100116156914714&" width="100%" height="311" frameborder="0" allowtransparency="true" scrolling="no"></iframe>
+            </div>
+        </div>
+ 
+        <script>
+            $(function() {
+                function showModal(id) {
+                    $(document.body).addClass('is-open-modal');
+                    $(id).addClass('is-open');
+                    console.log(id);
+                }
+ 
+                function hideModals() {
+                    $(document.body).removeClass('is-open-modal');
+                    $('.modal').removeClass('is-open');
+                }
+ 
+                $(".modal-open").on('click', function(e) {
+                    console.log(e);
+                    showModal('#modal1');
+                });
+ 
+                $(document).on('click', function(e) {
+                    if (!(
+                            ($(e.target).parents('.dialog').length) ||
+                            ($(e.target).hasClass('dialog')) ||
+                            ($(e.target).hasClass('modal-open')))) {
+                        hideModals();
+                    }
+                });
+ 
+            });
+        </script>
+    </div>
+    <script>
+        function myFunction() {
+            document.getElementById("myDropdown").classList.toggle("show");
+        }
+ 
+        window.onclick = function(event) {
+            if (!event.target.matches('.dropbtn')) {
+ 
+                var dropdowns = document.getElementsByClassName("dropdown-content");
+                var i;
+                for (i = 0; i < dropdowns.length; i++) {
+                    var openDropdown = dropdowns[i];
+                    if (openDropdown.classList.contains('show')) {
+                        openDropdown.classList.remove('show');
+                    }
+                }
+            }
+        }
+    </script>
+    <script>
+        var path = document.location.pathname;
+        if (document.location.pathname == '/kinoplay/kino/') {
+            window.location.href = "https://kinoplay.ahmedov.ml/kino" + document.location.search;
+        };
+        const showplayer = document.getElementById('show-player');
+        const Contentimg = document.getElementById('Content-img');
+        const israting = document.getElementById('israting');
+        const searchString = new URLSearchParams(window.location.search);
+        const mainContent = document.getElementById('mainContent');
+        const kpid = searchString.get('kpid');
+        const id = searchString.get('id');
+        const title = searchString.get('title');
+ 
+        function showPlayer(number) {
+            if (number == 0) {
+                Contentimg.innerHTML = '';
+                Contentimg.innerHTML = '<iframe class="iframe" src="/get_trailers?id=' + id + '"></iframe>';
+                israting.innerHTML = '';
+                israting.innerHTML = 'КиноПоиск | Трейлер';
+            } else {
+                fetch('/get_player?number=' + number + '&kpid=' + kpid)
+                    .then(res => res.json())
+                    .then(function(data) {
+                        Contentimg.innerHTML = '';
+                        Contentimg.innerHTML = '<iframe class="iframe" src="' + data.link + '" allowfullscreen seamless></iframe>';
+                        israting.innerHTML = '';
+                        israting.innerHTML = data.translation + ' | ' + data.quality;
+                    })
+                    .catch(function(error) {
+                        console.log(error);
+                    });
+            };
+        };
+ 
+        function showFiles() {
+            fetch('/get_files/test.php?type=movies&kpid=' + kpid)
+                .then(res => res.text())
+                .then(function(data) {
+                    Contentimg.innerHTML = '';
+                    Contentimg.innerHTML = data;
+                })
+                .catch(function(error) {
+                    console.log(error);
+                });
+        };
+ 
+        function getFiles(path) {
+            fetch('/get_files?' + path)
+                .then(res => res.text())
+                .then(function(data) {
+ 
+                    window.open("https://" + data, "KinoPlay", "menubar=yes");
+ 
+                })
+                .catch(function(error) {
+                    console.log(error);
+                });
+        };
+ 
+        let isInstalled = localStorage.getItem("pwaInstalled") === "1" || false;
+ 
+        if (window.matchMedia("(display-mode: standalone)").matches || window.navigator.standalone === true) {
+            // User is currently navigating on the PWA so yes it"s installed
+            localStorage.setItem("pwaInstalled", "1");
+            //       alert("1");
+            isInstalled = true;
+        } else {
+            //User is navigating in browser
+            window.addEventListener("beforeinstallprompt", () => {
+                localStorage.setItem("pwaInstalled", "0");
+                isInstalled = false;
+                //User can get an installation prompt meaning the app is not installed
+            });
+            window.addEventListener("onappinstalled", () => {
+                localStorage.setItem("pwaInstalled", "1");
+                isInstalled = true;
+            });
+        }
+    </script>
+    <script src="https://weblion777.github.io/hdvb_new.js" async></script>
+</body>
+ 
+</html>
