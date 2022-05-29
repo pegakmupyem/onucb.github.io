72ad67f271d0f2cc76e2965f9bd88d94d38b69a4
onucb <98803886+onucb@users.noreply.github.com>
2022-05-29 09:51:43 +0300

    Create test.js

---
+++ js/test.js
@@ -0,0 +1,20 @@
+window.hash = {
+  acthash: "f064df9af70e9fcbdae899bc4b626924"
+};
+const input = document.getElementById('txt');
+$(document).ready(function(){
+                 $("#txt").keypress(function(e){
+                   if(e.keyCode==13){
+                     if (MD5(input.value)==hash.acthash) {
+                       localStorage.removeItem('pass');
+                       localStorage.setItem('pass', JSON.stringify(hash.acthash));
+                       window.open('/search')
+                     } else {
+                       alert('📵 НЕправильный Пароль‼️ Пароль ДОЛЖЕН быть БЕЗ пробелов‼️');
+                       localStorage.removeItem('pass');
+                       localStorage.setItem('pass', JSON.stringify("noLLIeJl_Haxyū"));
+                       
+                     }
+                   }
+                 });
+             });
