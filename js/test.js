964894d0b3c36723fbf04bda7042b3b217149083
onucb <98803886+onucb@users.noreply.github.com>
2022-05-29 10:01:18 +0300

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,6 +1,9 @@
 window.hash = {
   acthash: "f064df9af70e9fcbdae899bc4b626924"
 };
+var localValue = localStorage.getItem('pass');
+console.log(localValue);
+var txt = localValue;
 const input = document.getElementById('txt');
 $(document).ready(function(){
                  $("#txt").keypress(function(e){
