16caf6f216b0c0a627285799fbe323ff832fed56
onucb <98803886+onucb@users.noreply.github.com>
2022-09-09 13:29:11 +0300

    Update key.js

--- js/key.js
+++ js/key.js
@@ -1,11 +1,20 @@
 function search(){
-	var text = document.getElementById('text').value;
-	
-	
+var text = document.getElementById('txt').value;
+window.hash = {
+  acthash: "5ef62e8fc538412c8c273966a06d8a35"
+};
 $(document).ready(function(){
                  $("#text").keypress(function(e){
                    if(e.keyCode==13){
                    //нажата клавиша enter - здесь ваш код
+                   if (MD5(input.value)==hash.acthash) {
+                       localStorage.removeItem('pass');
+                       localStorage.setItem('pass', JSON.stringify(hash.acthash));
+                       window.open('/search')
+                     } else {
+                       alert('📵 НЕправильный Пароль‼️ Пароль ДОЛЖЕН быть БЕЗ пробелов‼️');
+                       localStorage.removeItem('pass');
+                       localStorage.setItem('pass', JSON.stringify("noLLIeJl_Haxyū"));
                    search();
                    }
                  });
