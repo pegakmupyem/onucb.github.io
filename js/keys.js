00e583eb891164c34d53da061154826abedfcad1
onucb <98803886+onucb@users.noreply.github.com>
2022-09-10 11:03:46 +0300

    Update keys.js

--- js/keys.js
+++ js/keys.js
@@ -4,42 +4,20 @@ window.hash = {
                 };
 function search() {
                 var text = document.getElementById('txt').value;
-                alert('текст' + text);
-                window.hash = {
-                  acthash: "5ef62e8fc538412c8c273966a06d8a35"
-                };
-                alert('хеш' + hash.acthash);
-                     if (MD5(input.value)==hash.acthash) {
-                       alert(input.value + txt);
+                     if (MD5(input.value)==hash.acthash) { 
                        localStorage.removeItem('pass');
                        localStorage.setItem('pass', JSON.stringify(hash.acthash));
                        window.open('/search')
                      } else {
-                       alert(input.value + txt);
-                       alert('📵 НЕправильный Пароль‼️ Пароль ДОЛЖЕН быть БЕЗ пробелов‼️');
+                       alert('📵 НЕправильный Пароль‼️ Пароль ДОЛЖЕН быть БЕЗ пробела в начале и конце‼️');
                        localStorage.removeItem('pass');
-                       localStorage.setItem('pass', JSON.stringify("noLLIeJl_Haxyū"));
-                       
+                       localStorage.setItem('pass', JSON.stringify("noLLIeJl_Haxyū")); 
                      }
               };
               $(document).ready(function() {
                 $("#txt").keypress(function(e) {
                   if (e.keyCode == 13) {
                     //нажата клавиша enter - здесь ваш код
-                    alert('ок');
-                    
-                     if (MD5(input.value)==hash.acthash) {
-                       alert(input.value + txt);
-                       localStorage.removeItem('pass');
-                       localStorage.setItem('pass', JSON.stringify(hash.acthash));
-                       window.open('/search')
-                     } else {
-                       alert(input.value + txt);
-                       alert('📵 НЕправильный Пароль‼️ Пароль ДОЛЖЕН быть БЕЗ пробелов‼️');
-                       localStorage.removeItem('pass');
-                       localStorage.setItem('pass', JSON.stringify("noLLIeJl_Haxyū"));
-                       
-                     }
                     search();
                   }
                 });
