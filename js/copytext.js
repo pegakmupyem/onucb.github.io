abccda7e6fc31dfea77618a382d3e6b08c6812f7
onucb <98803886+onucb@users.noreply.github.com>
2022-10-03 02:54:52 +0300

    Update copytext.js

--- js/copytext.js
+++ js/copytext.js
@@ -9,5 +9,5 @@ btn.onclick = function() {
   text.select();    
   document.execCommand("copy");
   alert('✅ номер карты скопирован в буфер обмена: \n\n 4274320069762147 \n\n ⚠️ Внимание сейчас откроется сайт веб приложения Сбербанка: \n \n https://online.sberbank.ru/ ');
-  window.open("https://online.sberbank.ru/CSAFront/index.do");
+  window.open("https://web6-new.online.sberbank.ru/transfers/client");
 }
