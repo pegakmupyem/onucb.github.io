11bff865e3d01f2aae1397d01dd339f7c983d8ce
onucb <98803886+onucb@users.noreply.github.com>
2022-10-02 10:49:31 +0300

    Update alert.js

--- js/alert.js
+++ js/alert.js
@@ -1,5 +1,5 @@
-		var check = localStorage.getItem("check");
-		console.log(check);
+var check = localStorage.getItem("check");
+	console.log(check);
 		if (check == 2873143c9720ea7661247fba53180d53) {
   		 
   		  alert('Ваш ключ доступа устарел, купите новый ключ доступа, либо вы ввели его неправильно!');
